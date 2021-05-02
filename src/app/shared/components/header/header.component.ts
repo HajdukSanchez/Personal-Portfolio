3d8f5a1d4c95de164f0396fd28ff02fa03bce623
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-05-01 19:08:37 -0500

    :dizzy: Header transition
    Menu hamburguer transition

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  openMenu: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toogleMenu(): void {
+    this.openMenu = this.openMenu ? false : true;
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavMenuComponent
   ],
   imports: [
     CommonModule,

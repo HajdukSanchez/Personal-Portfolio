1a79a5e95e267a373983e1eeb65848a9d63b6e81
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-05-02 15:38:45 -0500

    :art: Header style

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
-
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    NavMenuComponent
   ],
   imports: [
     CommonModule,

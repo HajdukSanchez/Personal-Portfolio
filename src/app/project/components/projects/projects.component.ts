af438acdd95a7927a52cecd76cb24a94c7a675a5
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-05-06 18:00:39 -0500

    :boom: Media queries style

--- src/app/project/components/projects/projects.component.ts
+++ src/app/project/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-projects',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+
+  // Font awesome
+  faLock = faLock;
 
   title: string = 'Projects';
   description: string = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores voluptatibus itaque laboriosam sint voluptatem minima reprehenderit, facere aperiam praesentium ipsum?'

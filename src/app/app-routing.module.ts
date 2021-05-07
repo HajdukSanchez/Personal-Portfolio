21ec25185b5885e7856ce23e2f489bbcf17c2a5f
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-05-07 13:43:53 -0500

    :lipstick: Button download

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

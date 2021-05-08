8f541b7ed4ef64f5859ead16d297298241769e08
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-05-08 16:19:01 -0500

    :lipstick: Changes on style

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -9,6 +9,8 @@ export class HeaderComponent implements OnInit {
 
   openMenu: boolean = false;
 
+  cvPath: string = 'https://firebasestorage.googleapis.com/v0/b/jhs-portfolio.appspot.com/o/cv%2FCV-Jozek-Hajduk.pdf?alt=media&token=0026d87b-bbeb-4867-87a5-24560958b32f';
+
   constructor() { }
 
   ngOnInit(): void {

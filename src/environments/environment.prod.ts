f37c44f4c801585f68cf76a4d8916f941f9159d0
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-05-08 17:11:18 -0500

    :bug: Environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBoxcfH8qN_A2MldA1EygY0tMkh74CCaf4",
+    authDomain: "jhs-portfolio.firebaseapp.com",
+    databaseURL: "https://jhs-portfolio-default-rtdb.firebaseio.com",
+    projectId: "jhs-portfolio",
+    storageBucket: "jhs-portfolio.appspot.com",
+    messagingSenderId: "702701419962",
+    appId: "1:702701419962:web:78e1880d0fb483c77fb183"
+  },
 };
